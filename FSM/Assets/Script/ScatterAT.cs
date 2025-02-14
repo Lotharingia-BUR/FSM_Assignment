3eef965c1d82433a8c2b519de33499a6d586caff
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2025-02-13 22:00:51 -0500

    Starting work on code
    It keeps falling through the floor though

--- FSM/Assets/Script/ScatterAT.cs
+++ FSM/Assets/Script/ScatterAT.cs
@@ -1,21 +1,40 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class ScatterAT : ActionTask {
+		public BBParameter<List<Transform>> holePositions;
+        public BBParameter<Transform> target;
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
-			return null;
+		public float maxDectectionRadius;
+		private float currentDectectionRadius;
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			currentDectectionRadius = maxDectectionRadius;
+            return null;
 		}
 
 		//This is called once each time the task is enabled.
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
+			foreach(Transform holePosition in holePositions.value)
+			{
+				float distanceToHole = (holePosition.position - agent.transform.position).magnitude;
+
+                Debug.Log(distanceToHole);
+				if(currentDectectionRadius > distanceToHole)
+				{
+					currentDectectionRadius = distanceToHole;
+					target.value = holePosition;
+                }
+			}
 			EndAction(true);
 		}
 
