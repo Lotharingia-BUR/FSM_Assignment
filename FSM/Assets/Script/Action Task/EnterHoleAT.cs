33a576c8b84c983820d038909e60a8840b0f417c
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2025-02-19 10:52:46 -0500

    More Movment Stuff

--- FSM/Assets/Script/Action Task/EnterHoleAT.cs
+++ FSM/Assets/Script/Action Task/EnterHoleAT.cs
@@ -1,5 +1,6 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
@@ -16,6 +17,8 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
+			//Hide the Prairie Dog
+			agent.transform.position = new Vector3(100, 0, 100);
 			EndAction(true);
 		}
 
--- FSM/Assets/Script/Action Task/ExitHoleAT.cs
+++ FSM/Assets/Script/Action Task/ExitHoleAT.cs
@@ -1,11 +1,12 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class ExitHoleAT : ActionTask {
-
+		public BBParameter<Transform> target;
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
@@ -16,7 +17,8 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
+            agent.transform.position = target.value.position;
+            EndAction(true);
 		}
 
 		//Called once per frame while the action is active.
