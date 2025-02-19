31a661dd0b4fa685d8c56822a3aa668709d55e8b
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2025-02-19 10:25:40 -0500

    Movment FSM

--- FSM/Assets/Script/Action Task/RollTargetAT.cs
+++ FSM/Assets/Script/Action Task/RollTargetAT.cs
@@ -1,12 +1,16 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using System.Collections.Generic;
 using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
-	public class RollRandomAT : ActionTask {
-		public BBParameter<int> dice;
+	public class RollTargetAT : ActionTask {
+		public BBParameter<List<Transform>> holes;
+		public BBParameter<Transform> target;
+
+		private int randomHole;
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
@@ -17,14 +21,18 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			dice.value = Random.Range(0, 3);
-			EndAction(true);
+			
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			
-		}
+            randomHole = Random.Range(0, holes.value.Capacity);
+			if(holes.value[randomHole] != target.value)
+			{
+                target.value = holes.value[randomHole];
+                EndAction(true);
+            }
+        }
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
--- FSM/Assets/Script/Conditional Task/ifRandomEquals.cs
+++ FSM/Assets/Script/Conditional Task/ifRandomEquals.cs
@@ -5,7 +5,8 @@ using ParadoxNotion.Design;
 namespace NodeCanvas.Tasks.Conditions {
 
 	public class ifRandomEquals : ConditionTask {
-
+		public BBParameter<int> dice;
+		public int expectedValue;
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit(){
@@ -25,7 +26,11 @@ namespace NodeCanvas.Tasks.Conditions {
 		//Called once per frame while the condition is active.
 		//Return whether the condition is success or failure.
 		protected override bool OnCheck() {
-			return true;
+			if(dice.value == expectedValue)
+			{
+                return true;
+            }
+			return false;
 		}
 	}
 }
